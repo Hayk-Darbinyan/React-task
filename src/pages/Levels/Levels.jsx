bb0469fed8fb2d5482dee67d4e5b7dcc70469dcd
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2025-04-05 00:53:46 +0400

    bug fix

--- src/pages/Levels/Levels.jsx
+++ src/pages/Levels/Levels.jsx
@@ -5,23 +5,23 @@ const Levels = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="levels-container">
-      <h1 className="game-title">Memory Card Game</h1>
+    <div className="levelsContainer">
+      <h1 className="gameTitle">Memory Card Game</h1>
       <div className="levels">
         <h2>Choose Difficulty</h2>
-        <div className="level-cards">
-          <div className="level-card" onClick={() => navigate("/game/1")}>
-            <div className="level-icon">🐶</div>
+        <div className="levelCards">
+          <div className="level-Card" onClick={() => navigate("/game/1")}>
+            <div className="levelIcon">🐶</div>
             <h3>Easy</h3>
             <p>4x4 Grid</p>
           </div>
           <div className="level-card" onClick={() => navigate("/game/2")}>
-            <div className="level-icon">🦊</div>
+            <div className="levelIcon">🦊</div>
             <h3>Medium</h3>
             <p>6x6 Grid</p>
           </div>
           <div className="level-card" onClick={() => navigate("/game/3")}>
-            <div className="level-icon">🦉</div>
+            <div className="levelIcon">🦉</div>
             <h3>Hard</h3>
             <p>8x8 Grid</p>
           </div>
